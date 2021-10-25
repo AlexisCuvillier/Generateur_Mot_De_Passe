b200bafdbcd1e76ad9607b104b75b5c15a8ce630
AlexisCuvillier <alexis.cuvillier@protonmail.com>
2021-10-25 14:05:57 +0200

    finish

---
+++ GénératorPassword/Program.cs
@@ -0,0 +1,86 @@
+﻿using FomationCS;
+using System;
+
+namespace GénératorPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NB_MP = 10;
+
+             
+
+
+            // 1- Demander la longueurs du mot de passe (demandernombre) int longueurmp = ...
+             int longueurMp = outils.DemanderNOmbrePositifNonNull("Longueur du mot de passe : ");
+
+            int choixAlphabet = outils.DemanderNombreEntre("Entre un chiffre entre 1 et 4 pour un mot de passe avec :\n" +
+                 "1-minuscule\n" +
+                 "2-majuscule et minuscule\n" +
+                 "3-chiffre, majuscule et minuscule\n" +
+                 "4-caractère spéciaux, chiffre, majuscule et minuscules\n" +
+                 "Votre choix : ", 1, 4);
+          
+
+            // 2- alphabet(abc1234): 4 = '1a2d)
+            // 3- commenct choisir caractere aleatoire
+           
+
+            
+
+            string minuscule = "abcdefghijklmnopqrstuvwxyz";
+            string majuscule = minuscule.ToUpper() ;
+            string chiffre = "0123456789";
+            string caractereSpeciaux = "'(;.!?,~£$#{[]}";
+            string alphabet; 
+            string password = "";
+            Random rand = new Random();
+
+            if (choixAlphabet == 1)
+            {
+                alphabet = minuscule;
+            }
+            else if(choixAlphabet == 2)
+            {
+                alphabet = minuscule + majuscule;
+            }
+            else if(choixAlphabet == 3)
+            {
+                alphabet = minuscule + majuscule + chiffre;
+            }
+            else
+            {
+                alphabet = minuscule + majuscule + chiffre + caractereSpeciaux;
+            }
+
+
+           
+            int longueurAlpha = alphabet.Length;
+
+            for (int j = 0; j < NB_MP; j++)
+            {
+
+                password = "";
+                for (int i = 0; i < longueurMp; i++)
+                {
+                    int index = rand.Next(0, longueurAlpha);
+                    password += alphabet[index];
+
+                }
+                Console.WriteLine("Mot de passe n° "+(j+1)+" : " + password);
+            }
+
+            
+
+           
+
+            
+            // 4- generer le mp
+            // 5- Amelioration : choix alphabet
+
+        }
+
+
+    }
+}
---
+++ GénératorPassword/outils.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FomationCS
+{
+
+    // FomationCS.outils.DemanderNOmbrePositifNonNull
+    static class outils
+    {
+      public static int DemanderNOmbrePositifNonNull(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue, "ERROR : le nombre doit être positif et non nul");
+
+            /* int nombre = DemanderNombre(question);
+
+              if(nombre > 0)
+              {
+                  return(nombre);
+              }
+              Console.WriteLine("ERREUR : Le nombre doit être supèrieur à 0");
+              return DemanderNOmbrePositifNonNull(question); */
+        }
+
+      public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPersonnalise = null)
+        {
+
+            //     while(true)
+            //   {
+
+            int nombre = DemanderNombre(question);
+
+            if ((nombre >= min) && (nombre <= max))
+            {
+                // valide
+                return nombre;
+            }
+            if (messageErreurPersonnalise == null)
+            {
+            Console.WriteLine("ERREUR : Le nombre doit être compris entre " + min + " et " + max);
+            Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(messageErreurPersonnalise);
+            }
+           
+            return DemanderNombreEntre(question, min, max);
+
+
+            //     }
+
+
+        }
+
+      public static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+
+                Console.Write(question);
+                string response = Console.ReadLine();
+
+                try
+                {
+                    int responseInt = int.Parse(response);
+                    return responseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("Entrez un nombre correct");
+                }
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
